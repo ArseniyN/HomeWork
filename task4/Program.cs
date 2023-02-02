6696ae674607f1205f453ad5bbf9056ddf95572e
Arseniy <a.novogrodskiy@gmail.com>
2023-02-02 17:04:03 +0300

    Solved the task4

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.Write("Введите любое число, отличающееся от 1: ");
+int N = int.Parse(Console.ReadLine());
+int start = 1;
+if (N > 1)
+{
+    while (start <= N)
+    {
+        if (start % 2 == 0) Console.Write(start + ", ");
+        start++;
+    }
+}
+if(N <=1) Console.WriteLine("ошибка");
