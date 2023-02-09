1e62c1b78b87739512131ee1d9d9e8e8df2a678a
Arseniy <a.novogrodskiy@gmail.com>
2023-02-09 20:40:29 +0300

    сделал дз

--- task4/Program.cs
+++ task4/Program.cs
@@ -7,12 +7,14 @@ Console.Clear();
 Console.Write("Введите любое число, отличающееся от 1: ");
 int N = int.Parse(Console.ReadLine());
 int start = 1;
-if (N > 1)
-{
-    while (start <= N)
+while (start <= N)
     {
-        if (start % 2 == 0) Console.Write(start + ", ");
-        start++;
+    Console.Write(start + ", ");
+    start++;
     }
-}
+    if (start % 2 == 0) 
+    {
+        
+    }
+
 if(N <=1) Console.WriteLine("ошибка");
---
+++ task6/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+
+Console.Write("Введи число: ");
+int num = int.Parse(Console.ReadLine());
+string numInText = Convert.ToString(num);
+if (numInText.Length > 2)
+{
+  Console.WriteLine("третья цифра -> " + numInText[2]);
+}
+else 
+{
+  Console.WriteLine("-> третьей цифры нет");
+}
